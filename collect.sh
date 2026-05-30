1cca9e74d1867a5013ff22dc8b2c61bc40cb068d
Tristan <86117774+knockout101@users.noreply.github.com>
2026-05-29 20:29:35 -0700

    Added functionality to remove init.lua after collecting local files

--- collect.sh
+++ collect.sh
@@ -1,3 +1,6 @@
+# Collect path to this repo
 DOTFILES_PATH=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 cp ~/.config/nvim/lua/custom/plugins/* "$DOTFILES_PATH/nvim/"
+
+rm "$DOTFILES_PATH/nvim/init.lua"
